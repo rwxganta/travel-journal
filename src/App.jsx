9ac639a2cd9a3e9b2f26af3525e1025af471bab5
Luka Mendonça <lukamendoncadev@proton.me>
2023-07-19 17:16:50 -0300

    some adjusts && setting props on Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import data from './data.js';
 import Header from './components/Header';
 import Card from './components/Card';
 
 export default function App() {
+    const info = data.map((item) => {
+        return (<Card
+                    key={item.id}
+                    data={item}
+                />)
+    });
     return (
         <>
             <Header />
             <main>
                 <section className='wrapper flow-content'>
-                    <Card />
-                    <Card />
+                    {/* Still not completely clear how react pull the items out of the list */}
+                    { info }
                 </section>
             </main>
         </>
